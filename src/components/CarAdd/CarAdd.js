ee6fcbbb52db8bce8ba8c04c423b909f4a7f7b6f
erinbedri <erinbedri@gmail.com>
2023-02-01 10:27:22 +0100

    add car

--- src/components/CarAdd/CarAdd.js
+++ src/components/CarAdd/CarAdd.js
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import * as carService from "../../services/carService";
 
 export default function CarAdd() {
+    const [auth, setAuth] = useLocalStorage("auth", {});
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        manufacturer: "",
+        model: "",
+        category: "",
+        mileage: "",
+        year: "",
+        imageUrl: "",
+        price: "",
+        location: "",
+        description: "",
+
+        fname: auth.fname,
+        lname: auth.lname,
+        telNumber: auth.telNumber,
+    });
+
+    const isFormValid =
+        formData.manufacturer &&
+        formData.model &&
+        formData.category &&
+        formData.mileage &&
+        formData.year &&
+        formData.imageUrl &&
+        formData.price &&
+        formData.location &&
+        formData.description;
+
+    const [error, setError] = useState("");
+
+    const changeHandler = (e) => {
+        setFormData((oldData) => ({
+            ...oldData,
+            [e.target.name]:
+                e.target.type == "checkbox" ? e.target.checked : e.target.value,
+        }));
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        if (isFormValid) {
+            carService
+                .addCar(formData)
+                .then((res) => {
+                    if (res._ownerId) {
+                        navigate(`/catalog/${res._id}/details`);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        } else {
+            setError("All fields are mandatory!");
+            return;
+        }
+    };
+
     return (
         <section id="login" className="container">
             <h2 className="title">Add Car</h2>
+            {error && <div className="error">{error}</div>}
+
             <form className="form">
                 <label htmlFor="manufacturer">
                     <b>Manufacturer</b>
@@ -12,6 +79,7 @@ export default function CarAdd() {
                     type="text"
                     placeholder="Enter Manufacturer"
                     name="manufacturer"
+                    onChange={changeHandler}
                     required
                 />
                 <label htmlFor="model">
@@ -21,6 +89,7 @@ export default function CarAdd() {
                     type="text"
                     placeholder="Enter Model"
                     name="model"
+                    onChange={changeHandler}
                     required
                 />
                 <label htmlFor="category">
@@ -30,6 +99,7 @@ export default function CarAdd() {
                     type="text"
                     placeholder="Enter Category"
                     name="category"
+                    onChange={changeHandler}
                     required
                 />
                 <label htmlFor="mileage">
@@ -39,6 +109,7 @@ export default function CarAdd() {
                     type="number"
                     placeholder="Enter Mileage"
                     name="mileage"
+                    onChange={changeHandler}
                     min={0}
                     required
                 />
@@ -49,6 +120,7 @@ export default function CarAdd() {
                     type="number"
                     placeholder="Enter Year"
                     name="year"
+                    onChange={changeHandler}
                     min={1900}
                     max={2023}
                     required
@@ -60,6 +132,7 @@ export default function CarAdd() {
                     type="text"
                     placeholder="Enter Image URL"
                     name="imageUrl"
+                    onChange={changeHandler}
                     required
                 />
                 <label htmlFor="price">
@@ -69,6 +142,7 @@ export default function CarAdd() {
                     type="number"
                     placeholder="Enter Price"
                     name="price"
+                    onChange={changeHandler}
                     min={0}
                     required
                 />
@@ -79,6 +153,7 @@ export default function CarAdd() {
                     type="text"
                     placeholder="Enter Location"
                     name="location"
+                    onChange={changeHandler}
                     required
                 />
                 <label htmlFor="description">
@@ -87,10 +162,10 @@ export default function CarAdd() {
                 <textarea
                     id="description"
                     name="description"
+                    onChange={changeHandler}
                     rows={10}
-                    defaultValue={"                    "}
                 />
-                <button className="btn" type="submit">
+                <button onClick={submitHandler} className="btn" type="submit">
                     Add
                 </button>
             </form>
--- src/services/carService.js
+++ src/services/carService.js
@@ -5,3 +5,5 @@ const baseUrl = "http://localhost:3030/data/cars";
 export const getAll = () => request.get(baseUrl);
 
 export const getOne = (carId) => request.get(`${baseUrl}/${carId}`);
+
+export const addCar = (carData) => request.post(baseUrl, carData);

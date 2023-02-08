ea484a8bb3e419ea5b40b3275c1bc4a65d3fb025
erinbedri <erinbedri@gmail.com>
2023-02-08 15:16:21 +0100

    finished simple desing on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -27,37 +27,57 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-            </section>
 
-            <section className="container">
-                <h2 className="title">Welcome to Cars</h2>
+                <div className="home-desc container">
+                    <div className="desc-item">
+                        <i className="fa-solid fa-magnifying-glass" />
+                        <div className="desc-text">
+                            <h3>Find Your Dream Car</h3>
+                            <p>
+                                At Cars, we make buying and selling cars easier than ever. Whether you're looking to
+                                upgrade your current ride or sell your old car, we've got you covered. Our platform
+                                connects car sellers with potential buyers, enabling them to share detailed information
+                                about their vehicles and contact each other directly.
+                            </p>
+                        </div>
+                    </div>
 
-                <p>
-                    At Cars, we make buying and selling cars easier than ever. Whether you're looking to upgrade your
-                    current ride or sell your old car, we've got you covered. Our platform connects car sellers with
-                    potential buyers, enabling them to share detailed information about their vehicles and contact each
-                    other directly.
-                </p>
+                    <div className="desc-item">
+                        <i className="fa-solid fa-car-on" />
+                        <div className="desc-text">
+                            <h3>Discover Your Next Dream Car </h3>
+                            <p>
+                                Explore a wide range of cars from classic models to the latest releases. Browse our
+                                listings and find the perfect vehicle for you. Our detailed listings include all the
+                                information you need to make an informed decision, including photos, descriptions, and
+                                seller contact information.
+                            </p>
+                        </div>
+                    </div>
 
-                <h3>Discover Your Next Dream Car </h3>
-                <p>
-                    Explore a wide range of cars from classic models to the latest releases. Browse our listings and
-                    find the perfect vehicle for you. Our detailed listings include all the information you need to make
-                    an informed decision, including photos, descriptions, and seller contact information.
-                </p>
+                    <div className="desc-item">
+                        <i className="fa-solid fa-sack-dollar" />
+                        <div className="desc-text">
+                            <h3>Sell Your Car with Ease </h3>
+                            <p>
+                                Selling your car has never been easier. Simply create a listing and share the details of
+                                your vehicle. Our platform allows you to reach a large audience of potential buyers,
+                                helping you get the best price for your car.
+                            </p>
+                        </div>
+                    </div>
 
-                <h3>Sell Your Car with Ease </h3>
-                <p>
-                    Selling your car has never been easier. Simply create a listing and share the details of your
-                    vehicle. Our platform allows you to reach a large audience of potential buyers, helping you get the
-                    best price for your car.
-                </p>
-
-                <h3>Join the Cars Community</h3>
-                <p>
-                    Join our community of car enthusiasts and start buying and selling cars today. Register now to
-                    access all of our features and start making connections in the automotive world.
-                </p>
+                    <div className="desc-item">
+                        <i className="fa-solid fa-person-rays" />
+                        <div className="desc-text">
+                            <h3>Join the Cars Community</h3>
+                            <p>
+                                Join our community of car enthusiasts and start buying and selling cars today. Register
+                                now to access all of our features and start making connections in the automotive world.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     );

4403a38216c0ac7d82d0a0ef243677261dfd79be
erinbedri <erinbedri@gmail.com>
2023-02-09 12:55:37 +0100

    fixed navlinks to be active

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,27 +12,32 @@ export default function Header() {
         <header id="header" className="container">
             <div className="header-wrapper">
                 <div className="header-logo">
-                    <Link to="/">
+                    <NavLink to="/">
                         <img src={require("../../images/cars_logo.png")} alt="logo" />
-                    </Link>
+                    </NavLink>
                 </div>
 
                 <div className="header-controls">
+                    <NavLink to="/" activeclassname="active">
+                        Home
+                    </NavLink>
+                    <NavLink to="/catalog" end activeclassname="active">
+                        Catalog
+                    </NavLink>
                     {user.accessToken ? (
                         <>
-                            <NavLink to="#" activeclassname="active">
-                                <i className="fa-solid fa-heart" />
+                            <NavLink to="/catalog/car/add" activeclassname="active">
+                                Sell Car
                             </NavLink>
-                            <NavLink to="#">
-                                <i className="fa-solid fa-user" />
+                            <NavLink to="/catalog/owner" activeclassname="active">
+                                My Cars
                             </NavLink>
-                            <NavLink to="/catalog/car/add">Add Car</NavLink>
-                            <NavLink to="/catalog/owner">My Cars</NavLink>
-                            <NavLink to="/logout">Logout</NavLink>
+                            <NavLink to="/logout" activeclassname="active">
+                                Logout
+                            </NavLink>
                         </>
                     ) : (
                         <>
-                            <NavLink to="/catalog">Catalog</NavLink>
                             <NavLink to="/login">Sign In</NavLink>
                             <NavLink to="/register">Sign Up</NavLink>
                         </>
@@ -42,3 +47,12 @@ export default function Header() {
         </header>
     );
 }
+
+/*
+                            <NavLink to="#">
+                                <i className="fa-solid fa-heart" />
+                            </NavLink>
+                            <NavLink to="#">
+                                <i className="fa-solid fa-user" />
+                            </NavLink>
+*/

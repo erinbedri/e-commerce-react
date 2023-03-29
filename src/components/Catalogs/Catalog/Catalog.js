5ed573c8cbad5c860939dce8e0e9b66c833c62fe
erinbedri <erinbedri@gmail.com>
2023-03-29 19:16:49 +0200

    extracted catalog list into separate component

--- src/components/Catalogs/Catalog/Catalog.js
+++ src/components/Catalogs/Catalog/Catalog.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 import "./catalog.css";
 import * as carService from "../../../services/carService";
-import CarItem from "../../Cars/CarItem/CarItem";
 import useLoading from "../../../hooks/useLoading";
 import Error from "../../common/Error/Error";
 import Search from "../Search/Search";
 import Pagination from "../Pagination/Pagination";
+import CatalogList from "../CatalogList/CatalogList";
 
 export default function Catalog() {
     const { isLoading, loading } = useLoading(true);
@@ -114,54 +113,9 @@ export default function Catalog() {
         <>
             <Search handleSearch={handleSearch} />
 
-            <section id="catalog" className="container">
-                {cars.length > 0 ? (
-                    <div className="catalog-list">
-                        <div className="catalog-header">
-                            <span></span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Manufacturer
-                            </span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Model
-                            </span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Category
-                            </span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Mileage
-                            </span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Year
-                            </span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Price
-                            </span>
-                            <span onClick={handleSortBy}>
-                                <i className="fa-solid fa-sort" /> Location
-                            </span>
-                        </div>
+            <CatalogList cars={cars} handleSortBy={handleSortBy} />
 
-                        {cars.map((c) => (
-                            <Link to={`/catalog/${c._id}/details`} key={c._id} className="catalog-link">
-                                <CarItem {...c} />
-                            </Link>
-                        ))}
-                    </div>
-                ) : (
-                    <>
-                        <h3 style={{ textAlign: "center", marginTop: "2rem" }}>
-                            There are no cars in the catalog right now!
-                        </h3>
-                        <p style={{ textAlign: "center", marginTop: "1rem" }}>
-                            <Link to={"/catalog/car/add"}>
-                                <b>Sell cars</b>
-                            </Link>{" "}
-                            now.
-                        </p>
-                    </>
-                )}
-
+            {cars.length > 0 && (
                 <Pagination
                     handleFirstPage={handleFirstPage}
                     handlePreviousPage={handlePreviousPage}
@@ -174,7 +128,7 @@ export default function Catalog() {
                     handlePageSelector={handlePageSelector}
                     pageSize={pageSize}
                 />
-            </section>
+            )}
         </>
     );
 }
---
+++ src/components/Catalogs/CatalogList/CatalogList.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import CarItem from "../../Cars/CarItem/CarItem";
+
+export default function CatalogList({ cars, handleSortBy }) {
+    return (
+        <section id="catalog" className="container">
+            {cars.length > 0 ? (
+                <div className="catalog-list">
+                    <div className="catalog-header">
+                        <span></span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Manufacturer
+                        </span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Model
+                        </span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Category
+                        </span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Mileage
+                        </span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Year
+                        </span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Price
+                        </span>
+                        <span onClick={handleSortBy}>
+                            <i className="fa-solid fa-sort" /> Location
+                        </span>
+                    </div>
+
+                    {cars.map((c) => (
+                        <Link to={`/catalog/${c._id}/details`} key={c._id} className="catalog-link">
+                            <CarItem {...c} />
+                        </Link>
+                    ))}
+                </div>
+            ) : (
+                <>
+                    <h3 style={{ textAlign: "center", marginTop: "2rem" }}>
+                        There are no cars in this list right now!
+                    </h3>
+                    <p style={{ textAlign: "center", marginTop: "1rem" }}>
+                        <Link to={"/catalog/car/add"}>
+                            <b>Sell cars</b>
+                        </Link>{" "}
+                        now.
+                    </p>
+                </>
+            )}
+        </section>
+    );
+}
